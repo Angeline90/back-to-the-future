b2624eee3de74ac75c8277177b75aed2ee5c7be3
Angeline90 <angeline.gauron@gmail.com>
2023-04-10 16:10:13 +0200

    add decimal places

--- time.php
+++ time.php
@@ -5,5 +5,5 @@ $presentTime = new DateTime('now', $timeZone);
 $destinationTime = new DateTime('2050-12-12 12:24:00', $timeZone);
 $diff = $presentTime->diff($destinationTime);
 $minutes = $diff->days * 24 * 60 + $diff->h * 60 + $diff->i;
-$carburant = floor($minutes/10000);
+$carburant = round($minutes/10000,2);
 ?>
